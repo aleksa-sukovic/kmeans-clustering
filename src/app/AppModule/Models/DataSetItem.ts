08a3abfcd659d95b221e0e1fc855dd7549a70b38
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-03-03 17:29:29 +0100

    Adds distanceFromCluster method to DataSetItem model

--- src/app/AppModule/Models/DataSetItem.ts
+++ src/app/AppModule/Models/DataSetItem.ts
@@ -1,3 +1,5 @@
+import { Cluster } from './Cluster';
+
 export class DataSetItem
 {
     private values: number[];
@@ -7,8 +9,30 @@ export class DataSetItem
         this.values = values || [];
     }
 
+    distanceFromCluster(cluster: Cluster): number
+    {
+        let centroid = cluster.getCentroid();
+        let euclideanSum = 0;
+
+        for (let valueIndex = 0; valueIndex < centroid.getValues().length; valueIndex++) {
+            euclideanSum += Math.pow(
+                centroid.getValues()[valueIndex] - this.values[valueIndex],
+                2
+            );
+        }
+
+        return euclideanSum;
+    }
+
     getValues()
     {
         return this.values;
+    }
+
+    static createFromItem(item: DataSetItem): DataSetItem
+    {
+        let dataSetItem = new DataSetItem(Array.from(item.getValues()));
+
+        return dataSetItem;
     }
 }
