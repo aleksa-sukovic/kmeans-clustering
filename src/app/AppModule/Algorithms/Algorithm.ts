1c4ba55370885c74d9a6d5dcd6f1a138f52eeb1c
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-03-03 17:31:09 +0100

    Adds Algorithm base class

---
+++ src/app/AppModule/Algorithms/Algorithm.ts
@@ -0,0 +1,32 @@
+import { DataSet } from '../Models/DataSet';
+import { Cluster } from '../Models/Cluster';
+
+export abstract class Algorithm
+{
+    protected currentIteration: number;
+    protected complete: boolean;
+
+    protected abstract initialize(): Cluster[];
+    protected abstract iterate(): Cluster[];
+    protected abstract maxIterations(): number;
+
+    constructor()
+    {
+        this.currentIteration = 0;
+        this.complete = false;
+    }
+
+    public nextIteration(): Cluster[]
+    {
+        let clusters = this.currentIteration == 0 ? this.initialize() : this.iterate();
+
+        this.currentIteration++;
+
+        return clusters;
+    }
+
+    public hasNextIteration(): boolean
+    {
+        return !this.complete && (this.currentIteration < this.maxIterations());
+    }
+}
