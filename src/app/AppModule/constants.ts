316ec572fbf6ff3ee094369e8203a54a8595d309
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-03-03 13:25:11 +0100

    Fixes labels declaration in Constants class

--- src/app/AppModule/constants.ts
+++ src/app/AppModule/constants.ts
@@ -11,8 +11,8 @@ export class Constants
     static availableDataSets(): Array<{ name: string, label: string }>
     {
         return [
-            { name: 'Wine', label: Constants.dataSetLabels().IRIS },
-            { name: 'Iris Flowers', label: Constants.dataSetLabels().WINE }
+            { name: 'Wine', label: Constants.dataSetLabels().WINE },
+            { name: 'Iris Flowers', label: Constants.dataSetLabels().IRIS }
         ];
     }
 }
