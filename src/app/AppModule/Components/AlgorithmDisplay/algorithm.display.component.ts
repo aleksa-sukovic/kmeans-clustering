a913ad51d1f7f55628f0e275308db7386b4d6322
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-03-04 12:09:29 +0100

    Adds testing cluster centroid drawing in algorithm display component

--- src/app/AppModule/Components/AlgorithmDisplay/algorithm.display.component.ts
+++ src/app/AppModule/Components/AlgorithmDisplay/algorithm.display.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Algorithm } from '../../Algorithms/Algorithm';
 import { Cluster } from '../../Models/Cluster';
 import { DataSetItem } from '../../Models/DataSetItem';
+import { DimensionReductionService } from '../../Services/DimensionReductionService';
 
 @Component({
     selector: 'algorithm-display',
@@ -12,14 +13,19 @@ export class AlgorithmDisplayComponent implements OnInit
 {
     @Input() algorithm: Algorithm;
 
-    public width: number = 500;
+    public width: number = 700;
     public height: number = 500;
+    public scale: number = 2;
 
     private canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
 
+    constructor(private reductionService: DimensionReductionService) {  }
+
     public draw(): void
     {
+        this.context.clearRect(0, 0, this.width, this.height);
+
         this.algorithm.getClusters().forEach(cluster => {
             this.drawCluster(cluster);
         });
@@ -29,7 +35,7 @@ export class AlgorithmDisplayComponent implements OnInit
     {
         this.drawCentroid(cluster);
 
-        cluster.getItems().forEach(dataItem => this.drawItem(cluster, dataItem));
+        // cluster.getItems().forEach(dataItem => this.drawItem(cluster, dataItem));
     }
 
     protected drawCentroid(cluster: Cluster): void
@@ -41,9 +47,49 @@ export class AlgorithmDisplayComponent implements OnInit
 
     protected drawItem(cluster: Cluster, item: DataSetItem): void
     {
-        this.context.beginPath();
+        this.context.fillStyle = this.getClusterColor(cluster);
+        let coordinates        = this.getItemCoordinates(cluster, item);
 
-        this.context.closePath();
+        this.context.fillRect(coordinates.x, coordinates.y, 10, 10);
+    }
+
+    protected getClusterColor(cluster: Cluster): string
+    {
+        switch(cluster.getId() % 4) {
+            case 0: return '#4286f4';
+            case 1: return '#ff56ff';
+            case 2: return '#ff3a3a';
+            case 3: return '#ffc339';
+            default: '';
+        }
+    }
+
+    protected getItemCoordinates(cluster: Cluster, item: DataSetItem): { x: number, y: number }
+    {
+        let reduced = this.reductionService.reduceNumbers(item.getValues(), 2);
+
+        switch(cluster.getId() % 4) {
+            case 0: return {
+                x: Math.abs((this.width / 2 - reduced[0]) % (this.width / 2)),
+                y: Math.abs((this.height / 2 - reduced[1]) % (this.height / 2))
+            };
+            case 1: return {
+                x: this.width - Math.abs((this.width / 2 - reduced[0]) % (this.width / 2)),
+                y: Math.abs((this.height / 2 - reduced[1]) % (this.height / 2))
+            };
+            case 2: return {
+                x: Math.abs((this.width / 2 - reduced[0]) % (this.width / 2)),
+                y: this.height - Math.abs((this.height / 2 - reduced[1]) % (this.height / 2))
+            }
+            case 3: return {
+                x: this.width - Math.abs((this.width / 2 - reduced[0]) % (this.width / 2)),
+                y: this.height - Math.abs((this.height / 2 - reduced[1]) % (this.height / 2))
+            }
+            default: return {
+                x: Math.abs((this.width / 2 - reduced[0]) % (this.width / 2)),
+                y: Math.abs((this.height / 2 - reduced[1]) % (this.height / 2))
+            }
+        };
     }
 
     ngOnInit(): void
